5c7d870161e2baaeb1cd9625e4e26fb64e9dbab1
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-08-15 21:09:33 +0300

    Разобрали маршруты

--- Django/mainproject/djangoproject/urls.py
+++ Django/mainproject/djangoproject/urls.py
@@ -1,25 +1,21 @@
-"""
-URL configuration for djangoproject project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from new_app.views import Page
+from django.urls import path, include
+from new_app.views import Page, NewCompany
+
+managers_patterns = [
+    path('', NewCompany.managers),
+    path('director/', NewCompany.managers, kwargs={"page": "director"}),
+    path('dev_manager/', NewCompany.managers, kwargs={"page": "dev_manager"}),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', Page.view),
-    
+    path('home/', 
+        Page.view, 
+        kwargs={"name": "Николай", "company": "Google"}),
+    path('index/', Page.view_by_get),
+    path('main/', NewCompany.main),
+    path('news/', NewCompany.news),
+    path('managers/', include(managers_patterns)),
 ]
--- Django/mainproject/new_app/views.py
+++ Django/mainproject/new_app/views.py
@@ -1,20 +1,87 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+from abc import ABC, abstractmethod
 
 class Page:
 
     @staticmethod
-    def view(request):
+    def view(request, name, company):
         return HttpResponse(f"""
         <!DOCTYPE html>
         <html>
             <head></head>
             <body>
                 <h1> Добро пожаловать! </h1>
-                <p> Меня зовут Николай и это моя первая страница на django! </p>
+                <p> Меня зовут <b>{name}</b> и это моя первая страница на django! </p>
+                <p> Я python-разработчик компании <b>{company}</b></p>
             </body>
         </html>
         """)
     
-    def view_html(request):
-        return render(request, 'home.html')
+    @staticmethod
+    def view_by_get(request):
+        # http://127.0.0.1:8000/index/?name=John&company=Apple
+        name = request.GET.get("name", "Undefind")
+        company = request.GET.get("company", "Undefind")
+
+        return HttpResponse(f"""
+        <!DOCTYPE html>
+        <html>
+            <head></head>
+            <body>
+                <h1> Добро пожаловать! </h1>
+                <p> Меня зовут <b>{name}</b> и это моя первая страница на django! </p>
+                <p> Я python-разработчик компании <b>{company}</b></p>
+            </body>
+        </html>
+        """)
+    
+
+class TemplateWebSite(ABC):
+
+    @abstractmethod
+    def main(request):
+        return HttpResponseNotFound(f"""
+        <!DOCTYPE html>
+        <html>
+            <head></head>
+            <body>
+                <h1> 404 ERROR: PAGE NOT FOUND </h1>
+            </body>
+        </html>
+        """)
+    
+    @abstractmethod
+    def news(request):
+        return HttpResponseNotFound("Not Found")
+
+    @abstractmethod
+    def about(request):
+        return HttpResponseNotFound("Not Found")
+
+    @abstractmethod
+    def managers(request):
+        return HttpResponseNotFound("Not Found")
+    
+
+class NewCompany(TemplateWebSite):
+
+    def main(request):
+        return render(request, 'main.html', {
+            "name": "Google",
+        })
+
+    def news(request):
+        
+        return render(request, 'news.html', {
+            "news_list": ["Новость 1", "Новость 2", "Новость 3"]
+        })
+
+    def about(request):
+        return render(request, 'about.html')
+
+    def managers(request, page):
+        if not page:
+            return render(request, f'managers/managers.html')
+
+        return render(request, f'managers/{page}.html')
