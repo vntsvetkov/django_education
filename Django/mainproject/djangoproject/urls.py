6474ad34ff77e1d29072555497a829c3f986614e
nv.tsvetkov <nv.cvetkov@tensor.ru>
2023-08-18 10:47:27 +0300

    Добавил обработку неправильных адресов

--- Django/mainproject/djangoproject/urls.py
+++ Django/mainproject/djangoproject/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from new_app.views import Page, NewCompany
 
 managers_patterns = [
     path('', NewCompany.managers, kwargs={"page": ""}, name="managers"),
     path('director/', NewCompany.managers, kwargs={"page": "director"}, name="director"),
     path('dev_manager/', NewCompany.managers, kwargs={"page": "dev_manager"}, name="dev_manager"),
+    re_path(r'\w+', NewCompany.managers, kwargs={"page": ""}, name="managers"),
 ]
 
 urlpatterns = [
