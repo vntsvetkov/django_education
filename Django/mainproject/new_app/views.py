bf643fe8e4f06d07f460ebbe9cd80d2c6a068d83
nv.tsvetkov <nv.cvetkov@tensor.ru>
2023-09-01 16:17:40 +0300

    Исправил всплывающе окно

--- Django/mainproject/new_app/views.py
+++ Django/mainproject/new_app/views.py
@@ -140,8 +140,14 @@ class FormManage:
 
         time.sleep(1)
         return render(request, 'main.html', {
-                "message": "Вы подписаны на новостную рассылку",
-                "subscribe": True,
+                "subscribe": """ <div class="alert alert-success d-flex alert-dismissible fade show" role="alert">
+                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                                    </svg>
+                                    <strong> Подписка на новости успешно оформлена </strong>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                 </div>
+                             """
             })
 
 
