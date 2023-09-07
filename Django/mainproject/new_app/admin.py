e2744e790500b9916a0f214f54b1f6c006d71a19
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-09-07 20:58:25 +0300

    Добавили модели для реализации связей

--- Django/mainproject/new_app/admin.py
+++ Django/mainproject/new_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Article
 from .models import MailingAddress
+from .models import Person
 
 
 admin.site.register(Article)
 admin.site.register(MailingAddress)
+admin.site.register(Person)
--- Django/mainproject/new_app/models.py
+++ Django/mainproject/new_app/models.py
@@ -5,6 +5,7 @@ import datetime
 # Статья
 class Article(models.Model):
     title = models.CharField(max_length=30, default="Без названия")
+    # Необходимо связать автора с экземпляром Person
     author = models.CharField(max_length=20, default="Неизвестный автор")
     date = models.DateField(default=datetime.date.today())
     description = models.TextField(default="Статья в разработке")
@@ -12,6 +13,15 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+
+# Тема статьи
+class Theme(models.Model):
+    ...
+
+
+# Многие ко многим 
+class ArticlesThemes(models.Model):
+    ...
 
 
 # БД для хранения email адресов для рассылки
@@ -21,7 +31,18 @@ class MailingAddress(models.Model):
     def __str__(self):
         return self.email
 
+
 # Пользователи
-class Pesron(models.Model):
-    ...
+class Person(models.Model):
+    name = models.CharField(max_length=30, default="Неизвестно")
+    surname = models.CharField(max_length=30, default="Неизвестно")
+    phone = models.CharField(max_length=17, default="Неизвестно")
+    email = models.EmailField(unique=True, null=True, blank=True)
+    city = models.CharField(max_length=30, default="Неизвестно")
+    # Необходимо связать пользователя с его аккаунтом
+
+# Аккаунт
+class Account(models.Model):
+    login = models.CharField(max_length=30, default="Неизвестно")
+    password = models.CharField(max_length=30, default="Неизвестно")
 
